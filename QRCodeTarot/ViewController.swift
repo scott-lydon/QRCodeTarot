ad39c285b7b6d5b7f782b6226784cf99e20423ad
S <scottlydon18@gmail.com>
2022-04-03 19:53:49 -0700

    Add URL Scheme for QR codes, add Card and card symbol, along with the stack overflow link discussing deep linking, QR reader swift package.

--- QRCodeTarot/ViewController.swift
+++ QRCodeTarot/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
+
+/// We couldn't use a URL Scheme or the iphone camera to deep link to the
+///  app because it automatically plugs in the address in safari.
+///  https://stackoverflow.com/questions/71731173/how-to-make-the-iphone-camera-read-a-qr-code-and-go-to-my-app-instead-of-safari?noredirect=1#comment126767104_71731173
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
+extension Card {
+
+    enum Symbol: String, Codable {
+        case hearts
+        case clubs
+        case spades
+        case diamonds
+    }
+}
+
+
+struct Card {
+    let symbol: Symbol
+    let number: Int
+    let deckVersion: Int
+}
