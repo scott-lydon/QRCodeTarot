68e90ddbda0aacaf0bd1e9a79e221a2efd770d2d
S <scottlydon18@gmail.com>
2022-04-04 21:09:07 -0700

    Refactor Card to match the local JSON

--- QRCodeTarot/Card.swift
+++ QRCodeTarot/Card.swift
@@ -7,18 +7,57 @@
 
 import Foundation
 
-struct Card: Codable {
-    let symbol: Symbol
-    let number: Int
-    let deckVersion: Int
-}
 
-extension Card {
+// MARK: - Cards
+struct Cards: Codable {
+    let nhits: Int
+    let documentation: String
+    let cards: [Card]
 
-    enum Symbol: String, Codable {
-        case hearts
-        case clubs
-        case spades
-        case diamonds
+    func card(from cardKey: CardKey) -> Card? {
+        cards.first { $0.matches(key: cardKey) }
     }
 }
+
+// MARK: - Card
+struct Card: Codable {
+    let type: Importance
+    let name_short: String
+    let name: String
+    let value: String
+    let value_int: Int
+    let meaning_up: String
+    let meaning_rev: String
+    let desc: String
+    let suit: Suit?
+
+    func matches(key: CardKey) -> Bool {
+        suit == key.symbol.suit && key.number == value_int
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case name_short
+        case name
+        case value
+        case value_int
+        case meaning_up
+        case meaning_rev
+        case desc
+        case suit
+    }
+}
+
+enum Suit: String, Codable {
+    case cups
+    case pentacles
+    case swords
+    case wands 
+}
+
+enum Importance: String, Codable {
+    case major
+    case minor
+}
+
+
