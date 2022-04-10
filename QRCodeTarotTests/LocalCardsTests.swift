8eeca88af3678d740f63da29697f6d0d4496a0b1
S <scottlydon18@gmail.com>
2022-04-09 17:25:40 -0700

    Add tests for TarotCardMethod.

--- QRCodeTarotTests/LocalCardsTests.swift
+++ QRCodeTarotTests/LocalCardsTests.swift
@@ -38,4 +38,36 @@ class LocalCardsTests: XCTestCase {
         XCTAssertEqual(fiveHearts?.suit, .cups)
         XCTAssertEqual(fiveHearts?.value_int, 5)
     }
+
+    func testCardFromPath2SwordSpade() {
+        let localCards = Bundle.localCards
+        let fiveHearts = localCards.tarotCard(from: "4/swords/4/spades/2")
+        XCTAssertNotNil(fiveHearts)
+        XCTAssertEqual(fiveHearts?.suit, .swords)
+        XCTAssertEqual(fiveHearts?.value_int, 4)
+    }
+
+    func testCardFromPath2WandsSpade() {
+        let localCards = Bundle.localCards
+        let fiveHearts = localCards.tarotCard(from: "4/wands/4/spades/2")
+        XCTAssertNotNil(fiveHearts)
+        XCTAssertEqual(fiveHearts?.suit, .wands)
+        XCTAssertEqual(fiveHearts?.value_int, 4)
+    }
+
+    func testCardFromPath2SwordSpadev1() {
+        let localCards = Bundle.localCards
+        let fiveHearts = localCards.tarotCard(from: "4/swords/4/spades/1")
+        XCTAssertNotNil(fiveHearts)
+        XCTAssertEqual(fiveHearts?.suit, .swords)
+        XCTAssertEqual(fiveHearts?.value_int, 4)
+    }
+
+    func testCardFromPath2WandsSpadev1() {
+        let localCards = Bundle.localCards
+        let fiveHearts = localCards.tarotCard(from: "4/wands/4/spades/1")
+        XCTAssertNotNil(fiveHearts)
+        XCTAssertEqual(fiveHearts?.suit, .swords)
+        XCTAssertEqual(fiveHearts?.value_int, 4)
+    }
 }
