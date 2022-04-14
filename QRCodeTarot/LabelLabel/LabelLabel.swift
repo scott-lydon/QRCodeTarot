4db5705abb38702b96f6a052989b08b8f1494b31
S <scottlydon18@gmail.com>
2022-04-14 15:51:50 -0700

    Add LabelLabel

---
+++ QRCodeTarot/LabelLabel/LabelLabel.swift
@@ -0,0 +1,14 @@
+//
+//  LabelLabel.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 4/14/22.
+//
+
+import UIKit
+
+class LabelLabel: NibView {
+
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var bottomLabel: UILabel!
+}
