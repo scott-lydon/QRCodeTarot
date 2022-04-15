41f457485b0457891a798b58326cbc82479f0b54
S <scottlydon18@gmail.com>
2022-04-15 03:11:03 -0700

    Move MenuViewController.Row into its own file.

--- QRCodeTarot/ViewControllers/MenuViewController/MenuViewController.swift
+++ QRCodeTarot/ViewControllers/MenuViewController/MenuViewController.swift
@@ -49,38 +49,3 @@ class MenuViewController: UIViewController {
         return menuViewController
     }
 }
-
-extension MenuViewController {
-
-    enum Row: CaseIterable {
-
-        case tutorialRows(SubMenuViewController.TutorialRowType)
-        case tarotQRReader
-        case contact
-
-        init?(rawValue: String) {
-            if let tutorialRow =  SubMenuViewController.TutorialRowType(rawValue: rawValue) {
-                self = .tutorialRows(tutorialRow)
-            } else if rawValue == "Tarot QR Reader" {
-                self = .tarotQRReader
-            } else if rawValue == "Contact" {
-                self = .contact
-            } else {
-                return nil
-            }
-        }
-
-        var rawValue: String {
-            switch self {
-            case .tutorialRows(let tutorialRow): return tutorialRow.rawValue
-            case .tarotQRReader: return "Tarot QR Reader"
-            case .contact: return "Contact"
-            }
-        }
-
-        static var allCases: [MenuViewController.Row] {
-            SubMenuViewController.TutorialRowType.allCases.map { .tutorialRows($0)} +
-            [.tarotQRReader, .contact]
-        }
-    }
-}
