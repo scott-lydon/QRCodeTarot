bcfc10ca69cd8b32c0a450951ab30d3c4bb0286a
S <scottlydon18@gmail.com>
2022-04-15 01:52:52 -0700

    Add SubMenuViewController contents.

--- QRCodeTarot/ViewControllers/SubMenuViewController/SubMenuViewController.swift
+++ QRCodeTarot/ViewControllers/SubMenuViewController/SubMenuViewController.swift
@@ -6,8 +6,51 @@
 //
 
 import UIKit
+import TableMVVM
 
 class SubMenuViewController: UIViewController {
 
+    var dataSource: MenuDataSource = .init() {
+        didSet {
+            tableView.viewModel = dataSource
+        }
+    }
 
+    lazy var tableView: UITableMVVM = {
+        UITableMVVM(viewModel: dataSource)
+    }()
+
+    static func instantiate(with tutorialRow: TutorialRowType) -> SubMenuViewController {
+        let subMenuViewController: SubMenuViewController = UIStoryboard.vc()! // unitested
+        subMenuViewController.dataSource.section0.viewModel = tutorialRow.rawValue
+        return subMenuViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource.section0.cellsViewModels = TutorialRowType(rawValue: dataSource.section0.viewModel)!.detailViewModels
+        dataSource.section0.cellTapped = { rowText, indexPath in
+          //   navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: true)
+        }
+    }
+}
+
+extension SubMenuViewController {
+
+    enum TutorialRowType: String, CaseIterable {
+        case MagicTricks = "Magic Tricks"
+        case games = "Games"
+        case tarotCards = "Tarot Cards"
+
+        var detailViewModels: [String] {
+            switch self {
+            case .MagicTricks:
+                return ["magic trick 1", "magic trick 2", "magic trick 3"]
+            case .games:
+                return ["game 1", "game 2", "game 3", "game 4", "game 5"]
+            case .tarotCards:
+                return ["card 1", "card 2", "card 3", "card 4", "card 5"]
+            }
+        }
+    }
 }
