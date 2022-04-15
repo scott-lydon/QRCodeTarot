3b623aaf976d13bcd84370c05db93973bf85ae32
S <scottlydon18@gmail.com>
2022-04-15 03:11:18 -0700

    Move SubMenuViewController.TutorialRowType into its own file.

--- QRCodeTarot/ViewControllers/SubMenuViewController/SubMenuViewController.swift
+++ QRCodeTarot/ViewControllers/SubMenuViewController/SubMenuViewController.swift
@@ -42,23 +42,3 @@ class SubMenuViewController: UIViewController {
         }
     }
 }
-
-extension SubMenuViewController {
-
-    enum TutorialRowType: String, CaseIterable {
-        case MagicTricks = "Magic Tricks"
-        case games = "Games"
-        case tarotCards = "Tarot Cards"
-
-        var detailViewModels: [String] {
-            switch self {
-            case .MagicTricks:
-                return ["magic trick 1", "magic trick 2", "magic trick 3"]
-            case .games:
-                return ["game 1", "game 2", "game 3", "game 4", "game 5"]
-            case .tarotCards:
-                return ["card 1", "card 2", "card 3", "card 4", "card 5"]
-            }
-        }
-    }
-}
