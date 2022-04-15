2c87105e83b4980750639f819a59c45abc07be7d
S <scottlydon18@gmail.com>
2022-04-15 01:54:29 -0700

    Add DetailViewController.

---
+++ QRCodeTarot/ViewControllers/DetailsViewController/DetailsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailsViewController.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 4/15/22.
+//
+
+import UIKit
+import TableMVVM
+
+typealias DetailDataSource = TableDataSource1<
+    Section<
+        HeaderFooter<SimpleTextCell>,
+        ViewModelCell<DynamicDetailView>
+    >
+>
+
+class DetailsViewController: UIViewController {
+
+    var tutorial: String = ""
+
+    var dataSource: DetailDataSource = .init() {
+        didSet {
+            tableView.viewModel = dataSource
+        }
+    }
+
+    lazy var tableView: UITableMVVM = {
+        UITableMVVM(viewModel: dataSource)
+    }()
+
+    static func instantiate(with tutorial: String) -> DetailsViewController {
+        let detailsViewController: DetailsViewController = UIStoryboard.vc()!
+        detailsViewController.tutorial = tutorial
+        return detailsViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let viewModels: [DynamicDetailView.ViewModel] = .models(from: tutorial)
+        dataSource.section0.cellsViewModels = viewModels
+    }
+}
