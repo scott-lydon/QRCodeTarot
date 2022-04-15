8aeeb9885e597e0e02598bdb902a4037804203e4
S <scottlydon18@gmail.com>
2022-04-14 21:25:38 -0700

    Add HasViewModel conformance for LabelImage.

--- QRCodeTarot/LabelImage/LabelImage.swift
+++ QRCodeTarot/LabelImage/LabelImage.swift
@@ -6,8 +6,20 @@
 //
 
 import UIKit
+import TableMVVM
 
-class LabelImage: NibView {
+class LabelImage: NibView, HasViewModel {
+    typealias ViewModel = ImageLabel.ViewModel
+
+    var viewModel: ImageLabel.ViewModel = .fallBack {
+        didSet {
+            label.text = viewModel.text
+            imageView.image = viewModel.image
+        }
+    }
+
+
     @IBOutlet var label: UILabel!
     @IBOutlet var imageView: UIImageView!
 }
+
