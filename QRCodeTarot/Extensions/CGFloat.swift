025c67538bb88086330d392c3e742b20e9a97c34
S <scottlydon18@gmail.com>
2022-05-05 00:19:12 -0700

    Add CGFloat extension.

---
+++ QRCodeTarot/Extensions/CGFloat.swift
@@ -0,0 +1,23 @@
+//
+//  CGFloat.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 5/4/22.
+//
+
+import UIKit
+
+extension CGFloat {
+
+    func minus(_ number: Self) -> Self {
+        self - number
+    }
+
+    func divided(by denominator: Self) -> Self {
+        self / denominator
+    }
+
+    var square: CGSize {
+        CGSize(width: self, height: self)
+    }
+}
