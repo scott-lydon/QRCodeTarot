cb3a8e474d7df15d9039096c990963f56d403ff1
S <scottlydon18@gmail.com>
2022-05-06 13:24:49 -0700

    Add a title to the view controller.

--- QRCodeTarot/ViewControllers/MenuViewController/MenuViewController.swift
+++ QRCodeTarot/ViewControllers/MenuViewController/MenuViewController.swift
@@ -24,6 +24,7 @@ class MenuViewController: UIViewController {
         menuViewController.collectionView.register(ChoiceCell.self, forCellWithReuseIdentifier: ChoiceCell.className)
         menuViewController.greetingLabel.text = Date().greeting
         (UIApplication.shared.delegate as? AppDelegate)?.timeTracker = menuViewController
+        menuViewController.title = "Menu"
         return menuViewController
     }
 }
