0393b5a401265f8bb84c1baa775a7bf6652423cb
S <scottlydon18@gmail.com>
2022-05-09 21:00:25 -0700

    Update the SubMenuChoice code.

--- QRCodeTarot/SubMenuChoice.swift
+++ QRCodeTarot/SubMenuChoice.swift
@@ -6,7 +6,31 @@
 //
 
 import UIKit
+import TableMVVM
 
-class SubMenuChoice: NibView {
+class SubMenuChoice: NibView, HasViewModel {
 
+    @IBOutlet var title: UILabel!
+    @IBOutlet var details: UILabel!
+    @IBOutlet var viewToGradient: UIView!
+
+    var viewModel: ViewModel = .fallBack {
+        didSet {
+            self.title.text = viewModel.titleText
+            self.details.text = viewModel.detailText
+            self.viewToGradient.addGradiant()
+        }
+    }
+}
+
+
+extension SubMenuChoice {
+    struct ViewModel: HasFallBack {
+        let titleText: String
+        let detailText: String
+
+        static var fallBack: Self {
+            .init(titleText: "-", detailText: "-")
+        }
+    }
 }
