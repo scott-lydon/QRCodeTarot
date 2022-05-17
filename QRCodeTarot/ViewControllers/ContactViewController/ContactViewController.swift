eae3b3c76b95db6d67037914176d4db310a3d08a
S <scottlydon18@gmail.com>
2022-05-17 01:31:24 -0700

    Remove SimpleTextCell.

--- QRCodeTarot/ViewControllers/ContactViewController/ContactViewController.swift
+++ QRCodeTarot/ViewControllers/ContactViewController/ContactViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 /// TODO: Remodel. 
 class ContactViewController: UIViewController {
 
-    @IBOutlet var simpleText: SimpleTextCell!
-
     static func instantiate() -> ContactViewController {
         let viewController: ContactViewController = UIStoryboard.vc()!
         viewController.loadView()
-        viewController.simpleText.viewModel = "contact at..."
         return viewController
     }
 }
