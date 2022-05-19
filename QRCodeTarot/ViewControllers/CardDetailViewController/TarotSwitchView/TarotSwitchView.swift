00b88caf1903dea72011127d6b5300f22efed6af
S <scottlydon18@gmail.com>
2022-05-18 18:53:47 -0700

    Add Font extension.

--- QRCodeTarot/ViewControllers/CardDetailViewController/TarotSwitchView/TarotSwitchView.swift
+++ QRCodeTarot/ViewControllers/CardDetailViewController/TarotSwitchView/TarotSwitchView.swift
@@ -65,3 +65,33 @@ extension TarotSwitchView {
         }
     }
 }
+
+
+extension UIFont {
+
+    /// Provides a dynamic version of the current font.
+    var dynamic: UIFont {
+        UIFontMetrics(forTextStyle: .init(size: pointSize)).scaledFont(for: self)
+    }
+}
+
+extension UIFont.TextStyle {
+
+    /// Initializes the closes size.
+    init(size: CGFloat) {
+        switch size {
+        case 38...: self = .largeTitle
+        case 30..<38: self = .largeTitle
+        case 25..<30: self = .title1
+        case 21..<25: self = .title2
+        case 19..<21: self = .title3
+        case 17: self = .body
+        case 16: self = .callout
+        case 14..<16: self = .subheadline
+        case 13..<14: self = .footnote
+        case 12: self = .caption1
+        case 11: self = .caption2
+        default: self = .caption2
+        }
+    }
+}
