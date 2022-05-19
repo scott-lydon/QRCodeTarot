baddeb25769d4ecc32288172dad8890afc88a6e2
S <scottlydon18@gmail.com>
2022-05-18 22:45:05 -0700

    Add Tutorial Step view model.

---
+++ QRCodeTarot/Array.TutorialStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Array.TutorialStepViewModel.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 5/18/22.
+//
+
+import Foundation
+
+extension Array where Element == TutorialStepView.ViewModel {
+
+    var correctlyNumbered: Self {
+        enumerated().map { .init(image: $1.image, count: $0, stepDescription: $1.stepDescription) }
+    }
+}
