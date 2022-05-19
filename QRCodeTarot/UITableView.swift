564e4dbd202e2800c7d0d7c7c385bf246946e1fb
S <scottlydon18@gmail.com>
2022-05-18 23:36:39 -0700

    Add UITableView methods for index management.

---
+++ QRCodeTarot/UITableView.swift
@@ -0,0 +1,21 @@
+//
+//  UITableView.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 5/18/22.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func hasRowAtIndexPath(indexPath: IndexPath) -> Bool {
+        indexPath.section < self.numberOfSections && indexPath.row < self.numberOfRows(inSection: indexPath.section)
+    }
+
+    func indices(in section: Int) -> [IndexPath] {
+        let rowcount = numberOfRows(inSection: section)
+        guard rowcount > 0 else { return [] }
+        return (0..<rowcount).map { IndexPath(row: $0, section: section)}
+    }
+}
