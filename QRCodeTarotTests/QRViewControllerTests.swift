eb52cb53a06f4b1fa43758a802a3e69388d399e7
S <scottlydon18@gmail.com>
2022-06-03 11:43:20 -0700

    Delete commented code in QRViewControllerTests.

--- QRCodeTarotTests/QRViewControllerTests.swift
+++ QRCodeTarotTests/QRViewControllerTests.swift
@@ -23,18 +23,10 @@ class ViewControllerTests: XCTestCase {
         navigationController.pushViewController(viewController, animated: false)
         viewController.dispatcher = MockCanAsync()
         viewController.didRead(translatedQRCode: "/3/hearts/1")
-        let cardDetailsVC = viewController.navigationController?.viewControllers.last as? CardDetailViewController
         XCTAssertNotNil(viewController.navigationController)
         XCTAssertNotNil(viewController.navigationController?.viewControllers.last)
         viewController.readerDidCancel(viewController.readerVC)
         viewController.didRead(translatedQRCode: "")
-        // For some reason the view controller isn't being added to the views.
-//        XCTAssertNotEqual(viewController.navigationController?.viewControllers.last, viewController)
-//        // Make sure the navigation controller has Card Detail view controller.
-//        XCTAssertNotNil(viewController.navigationController?.viewControllers.compactMap { $0 as? CardDetailViewController }.first)
-//        XCTAssertNotNil(cardDetailsVC)
-//        XCTAssertEqual(cardDetailsVC?.titleLabel.text, "Three of Cups")
-//        XCTAssertEqual(cardDetailsVC?.detailsLabel.text, "Maidens in a garden-ground with cups uplifted, as if pledging one another")
     }
 
     func testCardKeyCases() {
