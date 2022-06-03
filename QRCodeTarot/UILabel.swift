803ad8f80d06891b6cdb639d396b4fb2a9610aae
S <scottlydon18@gmail.com>
2022-06-03 15:11:59 -0700

    Add to UILabel View Model.

--- QRCodeTarot/UILabel.swift
+++ QRCodeTarot/UILabel.swift
@@ -12,6 +12,7 @@ extension UILabel {
     /// Call this no more than once per UILabel instance.
     func setDynamicText() {
         font = font.dynamic
+
         adjustsFontForContentSizeCategory = true
         setContentCompressionResistancePriority(.required, for: .horizontal)
         setContentCompressionResistancePriority(.required, for: .vertical)
@@ -22,17 +23,21 @@ extension UILabel: HasViewModel {
 
     public var viewModel: ViewModel {
         get {
-            return .init(color: tintColor, font: font)
+            return .init(text: text ?? "-", color: tintColor, font: font, alignment: textAlignment)
         }
         set {
+            text = newValue.text
             textColor = newValue.color
             font = newValue.font
+            textAlignment = newValue.alignment
         }
     }
 
     public struct ViewModel: HasFallBack {
+        var text: String = "-"
         var color: UIColor = .white
         var font: UIFont = UIFont.inter(size: 16)
+        var alignment: NSTextAlignment = .left
 
         public static var fallBack: UILabel.ViewModel {
             .init()
