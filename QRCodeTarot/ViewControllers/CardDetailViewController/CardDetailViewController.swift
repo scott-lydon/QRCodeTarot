776ffbca609f22dc250599f0ba23e5783eae7c56
S <scottlydon18@gmail.com>
2022-06-06 09:02:50 -0700

    Refactor CardDetailViewController.

--- QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailViewController.swift
+++ QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailViewController.swift
@@ -8,36 +8,22 @@
 import UIKit
 import TableMVVM
 
-typealias CardImageCell = ViewModelCell<CardImageView>
-typealias CollapseTextCell = ViewModelCell<CollapsableLabelLabel>
-typealias TarotSwitchCell = ViewModelCell<TarotSwitchView>
-
 typealias CardDetailDataSource = TableDataSource5<
     SectionOneRow<ViewModelCell<UILabel>>,
-    SectionOneRow<CardImageCell>,
-    SectionOneRow<CollapseTextCell>,
-    SectionOneRow<TarotSwitchCell>,
+    SectionOneRow<ViewModelCell<CardImageView>>,
+    SectionOneRow<ViewModelCell<CollapsableLabelLabel>>,
+    SectionOneRow<ViewModelCell<TarotSwitchView>>,
     SectionNoHeader<ViewModelCell<LabelLabel>>
 >
 
-/// Needs to change to table View.  (Totally revamp.) with multiple sections. dark. 
 class CardDetailViewController: UIViewController {
 
-    var dataSource: CardDetailDataSource = .init() {
-        didSet {
-            tableView.viewModel = dataSource
-        }
-    }
-
-    lazy var tableView: UITableMVVM<CardDetailDataSource> = {
-        UITableMVVM(viewModel: dataSource)
-    }()
-
+    var tableView: UITableMVVM<CardDetailDataSource> = UITableMVVM(viewModel: .init())
     var card: Card! // unit tested.
 
     static func instantiate(card: Card) -> CardDetailViewController {
         let detailController: CardDetailViewController = UIStoryboard.vc()!
-        detailController.dataSource = .init(
+        detailController.tableView.viewModel = CardDetailDataSource(
             section0: SectionOneRow(
                 cellViewModel: .init(
                     text: card.name,
@@ -65,19 +51,17 @@ class CardDetailViewController: UIViewController {
         tableView.backgroundColor = .clear
         view.set(background: BackgroundView.zero.darkShade)
         view.inject(view: tableView)
-        dataSource.section2.cellViewModel.buttonTapped = { [weak self] in
-            self?.dataSource.section2.cellViewModel.labelLabelViewModel.lineCount = 0
-            self?.dataSource.section2.cellViewModel.buttonIsHidden = true
+        tableView.viewModel?.section2.cellViewModel.buttonTapped = { [weak self] in
+            self?.tableView.viewModel?.section2.cellViewModel.labelLabelViewModel.lineCount = 0
+            self?.tableView.viewModel?.section2.cellViewModel.buttonIsHidden = true
         }
-        dataSource.section3.cellViewModel.switchedToLeft = { [weak self] isEvolved in
-            print(self?.dataSource.section3.cellViewModel.isLeft as Any)
+        tableView.viewModel?.section3.cellViewModel.switchedToLeft = { [weak self] isEvolved in
             self?.tableView.updateViewModelWithoutTableUpdate { [weak self] in
                 guard let self = self else { return }
-                self.dataSource.section3.cellViewModel.isLeft = isEvolved
-                self.dataSource.section4.cellsViewModels = isEvolved ? self.card.evolvedViewModels : self.card.unevolvedViewModels
+                self.tableView.viewModel?.section3.cellViewModel.isLeft = isEvolved
+                self.tableView.viewModel?.section4.cellsViewModels = isEvolved ? self.card.evolvedViewModels : self.card.unevolvedViewModels
             }
             guard let self = self else { return }
-            print(self.dataSource.section3.cellViewModel.isLeft)
             self.tableView.reloadRows(at: self.tableView.indices(inSection: 3), with: .automatic)
         }
     }
