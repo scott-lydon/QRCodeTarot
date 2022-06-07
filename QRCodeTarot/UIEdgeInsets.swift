f943f63a08fb6adaf30572d38eb977cdc9873413
S <scottlydon18@gmail.com>
2022-06-06 18:24:33 -0700

    Add standard edge insets.

---
+++ QRCodeTarot/UIEdgeInsets.swift
@@ -0,0 +1,15 @@
+//
+//  UIEdgeInsets.swift
+//  QRCodeTarot
+//
+//  Created by Scott Lydon on 6/6/22.
+//
+
+import UIKit
+
+extension UIEdgeInsets {
+
+    static var standard: UIEdgeInsets {
+        .init(top: 0, left: 24, bottom: 0, right: 24)
+    }
+}
