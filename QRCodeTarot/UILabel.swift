bf1b571b1964c556d7f6f1ec9f8103120d51b57f
S <scottlydon18@gmail.com>
2022-06-07 13:06:06 -0700

    Move the with methods to extensions for UILabel.

--- QRCodeTarot/UILabel.swift
+++ QRCodeTarot/UILabel.swift
@@ -43,4 +43,14 @@ extension UILabel: HasViewModel {
             .init()
         }
     }
+
+    func with(text: String) -> Self {
+        self.text = text
+        return self
+    }
+
+    func with(lineCount: Int) -> Self {
+        self.numberOfLines = lineCount
+        return self
+    }
 }
--- QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailViewController.swift
+++ QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailViewController.swift
@@ -93,15 +93,3 @@ class CardDetailViewController: UIViewController {
         }
     }
 }
-
-extension UILabel {
-    func with(text: String) -> Self {
-        self.text = text
-        return self
-    }
-
-    func with(lineCount: Int) -> Self {
-        self.numberOfLines = lineCount
-        return self
-    }
-}
