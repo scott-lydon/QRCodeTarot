a449a3186d642da9f78e5e88b1990fc1d14a5c96
Scott Lydon <scottlydon18@gmail.com>
2022-09-25 19:22:38 -0400

    Add UiColors

--- QRCodeTarot/UIColor.swift
+++ QRCodeTarot/UIColor.swift
@@ -38,7 +38,29 @@ extension UIColor {
         UIColor(red: 0.11, green: 0.11, blue: 0.13, alpha: 1.00)
     }
 
+    /// out of focus color
+    ///  #151619
     static var darkBackground: UIColor {
         UIColor(red: 0.08, green: 0.09, blue: 0.10, alpha: 1.00)
+    }
+    
+    /// #F7F7F7
+    static var nearWhite: UIColor {
+        UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.00)
+    }
+    
+    /// #B3B3B3
+    static var deselectedGray: UIColor {
+        UIColor(red: 0.70, green: 0.70, blue: 0.70, alpha: 1.00)
+    }
+    
+    /// UIColor(red: 0.61, green: 0.62, blue: 0.67, alpha: 1.00)
+    /// #9B9FAB
+    static var gentleText: UIColor {
+        UIColor(red: 0.61, green: 0.62, blue: 0.67, alpha: 1.00)
+    }
+    
+    static var hex9B9FAB: UIColor {
+        UIColor(red: 0.61, green: 0.62, blue: 0.67, alpha: 1.00)
     }
 }
