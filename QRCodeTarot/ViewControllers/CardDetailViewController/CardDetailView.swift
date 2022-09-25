e8b24b49327a39f14eff622ae341af09cf6a07f0
Scott Lydon <scottlydon18@gmail.com>
2022-09-25 19:23:48 -0400

    Modify and mvp CardDetailView.

--- QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailView.swift
+++ QRCodeTarot/ViewControllers/CardDetailViewController/CardDetailView.swift
@@ -12,10 +12,11 @@ var uipaddingBoth: CGFloat { uipadding * 2 }
 
 struct CardDetailView: View {
     
-    @State var viewModel: CollapsableLabelLabel.ViewModel
+    @State var card: Card
+    
     @State var readMoreShow: Bool = true
-    @State var switchmodel: TarotSwitchView.ViewModel
-    @State var favoriteColor: Int = 0
+    @State var switchmodel: TarotSwitchView.ViewModel = .fallBack
+    @State var evolved: Int = 0
     
     var body: some View {
         GeometryReader { geometry in
@@ -27,26 +28,28 @@ struct CardDetailView: View {
                     .edgesIgnoringSafeArea(.all)
                 ScrollView {
                     VStack {
-                        Text("Two of Cups".capitalized)
+                        Text(card.name.capitalized)
                             .font(.largeTitle)
                             .foregroundColor(.white)
-                        Spacer(minLength: 30)
-                        Image(uiImage: .cardDemo)
+                            .padding(.bottom, 30)
+                        Image(uiImage: card.image!)
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 200)
                             .cornerRadius(15.0)
-                        Spacer(minLength: 20)
+                            .padding(.bottom, 20)
                         HStack {
                             Text("Description")
                                 .font(.title)
                                 .foregroundColor(.white)
                             Spacer()
                         }
-                        Spacer(minLength: 10)
-                        Text(String.lorem + String.lorem + String.lorem)
+                        .padding(.bottom, 5)
+                        Text(card.desc)
                             .lineLimit(readMoreShow ? 4 : nil)
-                            .foregroundColor(.white)
+                            .font(ui: .inter(size: 17).dynamic)
+                            .lineSpacing(6.0)
+                            .foregroundColor(Color(uiColor: .gentleText))
                             .padding(.bottom, 20)
                         if readMoreShow {
                             Button {
@@ -68,14 +71,24 @@ struct CardDetailView: View {
                         Color.gray
                             .frame(height: 1)
                             .padding(.top, 15)
-                    
-                        Picker("What is your favorite color?", selection: $favoriteColor) {
-                            Text("Evolved").tag(0)
-                            Text("Unevolved").tag(1)
+                        
+                        SegmentedPicker(
+                            items: ["Evolved", "Unevolved"],
+                            minHeight: 50,
+                            selection: $evolved
+                        )
+                        .padding([.top, .bottom])
+                        HStack {
+                            Text(evolved == 0 ? "Evolved" : "Unevolved")
+                                .font(.title)
+                                .foregroundColor(.white)
+                            Spacer()
                         }
-                        .pickerStyle(.segmented)
-                        
-                        
+                        .padding(.bottom, 5)
+                        Text(evolved == 0 ? card.evolved! : card.unevolved!)
+                            .lineSpacing(6.0)
+                            .font(ui: .inter(size: 17).dynamic)
+                            .foregroundColor(Color(uiColor: .gentleText))
                     }
                     .padding([.leading, .trailing], 25)
                 }
@@ -86,18 +99,33 @@ struct CardDetailView: View {
 
 struct CardDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        CardDetailView(viewModel: .init(labelLabelViewModel: LabelLabel.ViewModel(topText: "hello", bottomText: .lorem)), switchmodel: .fallBack)
-            .previewDevice("iPhone 14 Pro Max")
-        
-        CardDetailView(viewModel: .init(labelLabelViewModel: LabelLabel.ViewModel(topText: "hello", bottomText: .lorem)), switchmodel: .fallBack)
-            .previewInterfaceOrientation(.landscapeLeft)
-            .previewDevice("iPhone 14 Pro Max")
-        CardDetailView(viewModel: .init(labelLabelViewModel: LabelLabel.ViewModel(topText: "hello", bottomText: .lorem)), switchmodel: .fallBack)
-            .previewInterfaceOrientation(.portrait)
-            .previewDevice("iPhone SE (3rd generation)")
-        CardDetailView(viewModel: .init(labelLabelViewModel: LabelLabel.ViewModel(topText: "hello", bottomText: .lorem)), switchmodel: .fallBack)
-            .previewInterfaceOrientation(.landscapeLeft)
-            .previewDevice("iPhone SE (3rd generation)")
-        
+        CardDetailView(card: .random)
+            .allPreviews()
+    }
+}
+
+
+extension View {
+    
+    @inlinable public func font(ui: UIFont) -> some View {
+        font(Font(uiFont: ui))
+    }
+    
+    @inlinable func allPreviews() -> some View {
+        Group {
+            previewDevice("iPhone 14 Pro Max")
+            previewInterfaceOrientation(.landscapeLeft)
+                .previewDevice("iPhone 14 Pro Max")
+            previewInterfaceOrientation(.portrait)
+                .previewDevice("iPhone SE (3rd generation)")
+            previewInterfaceOrientation(.landscapeLeft)
+                .previewDevice("iPhone SE (3rd generation)")
+        }
+    }
+}
+
+public extension Font {
+    init(uiFont: UIFont) {
+        self = Font(uiFont as CTFont)
     }
 }
