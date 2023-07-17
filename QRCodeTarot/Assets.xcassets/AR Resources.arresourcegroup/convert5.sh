07ae0d25334002ce14b2e7394aaf468019bb75c4
Scott Lydon <scottlydon18@gmail.com>
2023-07-17 00:41:02 -0700

    Pointy hat tarot.

--- QRCodeTarot/Assets.xcassets/AR Resources.arresourcegroup/convert5.sh
+++ QRCodeTarot/Assets.xcassets/AR Resources.arresourcegroup/convert5.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-for dir in ./*_ar.arreferenceimage
-do
-    for png in "$dir"/*.png
-    do
-        echo "Converting $png to HEIC..."
-        heic=${png%.*}.heic
-        heif-convert "$png" "$heic" -q 50
-        rm "$png"
-        json="$dir/Contents.json"
-        jq '.images[0].filename |= sub(".png"; ".heic")' "$json" > temp && mv temp "$json"
-    done
-done
+# Part 1: Convert all .png files to .heic
+find . -name "*.png" -exec sh -c '
+  for img do
+    convert "$img" -quality 50 "${img%.png}.heic"
+    rm "$img"
+  done
+' sh {} +
+
+# Part 2: Update Contents.json files
+find . -name "Contents.json" -exec sh -c '
+  for json do
+    jq ".images[0].filename |= sub(\".png\"; \".heic\")" "$json" > temp && mv temp "$json"
+  done
+' sh {} +
